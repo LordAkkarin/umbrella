e3aab4d095fe07fcd31105e597ed801719dd3f9b
Johannes Donath <johannesd@evil-co.com>
2014-10-06 13:38:39 +0200

    Moved instruction registry to utility package.

--- map/src/main/java/umbrella/map/instruction/utility/IMapInstructionRegistry.java
+++ map/src/main/java/umbrella/map/instruction/utility/IMapInstructionRegistry.java
@@ -12,7 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package umbrella.map.instruction;
+package umbrella.map.instruction.utility;
+
+import umbrella.map.instruction.IMapInstruction;
 
 /**
  * Ensures that maps are built upon well known names.
