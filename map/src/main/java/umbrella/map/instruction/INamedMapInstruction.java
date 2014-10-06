a98c2cdef52373d0698deaf4c149e3476015b2f5
Johannes Donath <johannesd@evil-co.com>
2014-10-06 13:33:45 +0200

    Created named instruction definition.

---
+++ map/src/main/java/umbrella/map/instruction/INamedMapInstruction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package umbrella.map.instruction;
+
+/**
+ * Represents a named instruction.
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public interface INamedMapInstruction extends IMapInstruction {
+
+	/**
+	 * Returns an inversed version of the instruction.
+	 * @param name The name.
+	 * @return The instruction.
+	 */
+	public IMapInstruction getInverse (String name);
+
+	/**
+	 * Returns the field name.
+	 * @return The name.
+	 */
+	public String getName ();
+}
