61d00f4c51ce3989057c9fcf14a7e866bd8fadcd
Johannes Donath <johannesd@evil-co.com>
2014-10-06 15:59:14 +0200

    Added test for IOUtility.

---
+++ utility/src/test/java/umbrella/test/utility/IOUtilityTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package umbrella.test.utility;
+
+import lombok.Getter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import umbrella.utility.IOUtility;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+@RunWith (MockitoJUnitRunner.class)
+public class IOUtilityTest {
+
+	/**
+	 * Tests the closeQuietly method.
+	 */
+	@Test
+	public void testCloseQuietly () {
+		// try to close null
+		IOUtility.closeQuietly (null);
+
+		// create a test object
+		TestClass testClass = new TestClass ();
+
+		// close properly
+		IOUtility.closeQuietly (testClass);
+
+		// try to close again (causes an error)
+		IOUtility.closeQuietly (testClass);
+	}
+
+	/**
+	 * A closable test object.
+	 */
+	public static class TestClass implements Closeable {
+
+		/**
+		 * Indicates whether the test class has been closed.
+		 */
+		@Getter
+		private boolean closed = false;
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void close () throws IOException {
+			if (this.closed) throw new IOException ("Test Exception");
+			this.closed = true;
+		}
+	}
+}
