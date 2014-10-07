2897457ce05c8315f2a603bcff534027a6ab17c8
Johannes Donath <johannesd@evil-co.com>
2014-10-07 16:00:24 +0200

    Added name generator definition.

---
+++ analyzer/src/main/java/umbrella/generator/name/INameGenerator.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package umbrella.generator.name;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public interface INameGenerator {
+
+	/**
+	 * Generates a new field name.
+	 * @param previousName The previous name (if any).
+	 * @return The name.
+	 */
+	public String generateFieldName (String previousName);
+
+	/**
+	 * Generates a new invoke dynamic method name.
+	 * @param previousName The previous name (if any).
+	 * @return The name.
+	 */
+	public String generateInvokeDynamicMethodName (String previousName);
+
+	/**
+	 * Generates a new method name.
+	 * @param previousName The previous name (if any).
+	 * @return The name.
+	 */
+	public String generateMethodName (String previousName);
+
+	/**
+	 * Generates a new type name.
+	 * @param previousName The previous name (if any).
+	 * @return The name.
+	 */
+	public String generateTypeName (String previousName);
+}
