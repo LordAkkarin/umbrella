b5fc6c8551a1a85b2fc21b1f4dd9a06221b20aef
Johannes Donath <johannesd@evil-co.com>
2014-10-07 12:17:08 +0200

    Lots of fun changes including map loading and generation.

--- map/src/main/java/umbrella/map/GenericMap.java
+++ map/src/main/java/umbrella/map/GenericMap.java
@@ -15,6 +15,7 @@
 package umbrella.map;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import lombok.*;
@@ -24,14 +25,19 @@ import umbrella.map.instruction.utility.MapInstructionCategory;
 import umbrella.utility.IOUtility;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
  */
 public class GenericMap implements IMap {
+
+	/**
+	 * Stores the library version.
+	 */
+	public static final String VERSION;
 
 	/**
 	 * Stores all instructions.
@@ -45,6 +51,19 @@ public class GenericMap implements IMap {
 	@Setter (AccessLevel.PROTECTED)
 	@NonNull
 	private IMapInstructionRegistry instructionRegistry;
+
+	/**
+	 * Static Initializer
+	 */
+	static {
+		String version = null;
+
+		// get package version
+		try { version = GenericMap.class.getPackage ().getImplementationVersion (); } catch (Exception ex) { }
+
+		// store
+		VERSION = version;
+	}
 
 	/**
 	 * Constructs a new AbstractMap instance.
@@ -86,6 +105,92 @@ public class GenericMap implements IMap {
 
 		// merge map
 		this.merge (map);
+	}
+
+	/**
+	 * Constructs a new GenericMap instance.
+	 * @param registry The instruction registry.
+	 * @param inputStream The input stream.
+	 * @param close Indicates whether the stream shall be closed.
+	 */
+	protected GenericMap (@NonNull IMapInstructionRegistry registry, @NonNull InputStream inputStream, boolean close) {
+		this (registry);
+
+		// define variables
+		Scanner scanner = null;
+
+		// load data
+		try {
+			// open scanner
+			scanner = new Scanner (inputStream);
+
+			// initialize line number
+			int lineNumber = 0;
+
+			// load all lines
+			while (scanner.hasNextLine ()) {
+				// update line number
+				lineNumber++;
+
+				// extract line
+				String line = scanner.nextLine ();
+
+				// check line
+				if (line.isEmpty () || line.startsWith ("//")) continue;
+
+				// split mapping
+				List<String> elements = Splitter.on (' ').limit (4).splitToList (line);
+
+				// verify count
+				Preconditions.checkArgument (elements.size () == 4, "Mapping on line " + lineNumber + " does not contain exactly 4 elements");
+
+				// parse types
+				IMapInstruction original = this.getInstructionRegistry ().getElement (elements.get (0), elements.get (1));
+				IMapInstruction replacement = this.getInstructionRegistry ().getElement (elements.get (2), elements.get (3));
+
+				// add instruction
+				this.addInstruction (original, replacement);
+			}
+		} finally {
+			IOUtility.closeQuietly (scanner);
+			if (close) IOUtility.closeQuietly (inputStream);
+		}
+	}
+
+	/**
+	 * Constructs a new GenericMap instance.
+	 * @param registry The instruction registry.
+	 * @param inputStream The input stream.
+	 */
+	public GenericMap (@NonNull IMapInstructionRegistry registry, @NonNull InputStream inputStream) {
+		this (registry, inputStream, false);
+	}
+
+	/**
+	 * Constructs a new GenericMap instance.
+	 * @param inputStream The input stream.
+	 */
+	public GenericMap (@NonNull InputStream inputStream) {
+		this (IMapInstructionRegistry.DEFAULT, inputStream);
+	}
+
+	/**
+	 * Constructs a new GenericMap instance.
+	 * @param registry The instruction registry.
+	 * @param file The file.
+	 * @throws IOException Occurs if reading from the file is not possible.
+	 */
+	public GenericMap (@NonNull IMapInstructionRegistry registry, @NonNull File file) throws IOException {
+		this (registry, (new FileInputStream (file)), true);
+	}
+
+	/**
+	 * Constructs a new GenericMap instance.
+	 * @param file The file.
+	 * @throws IOException Occurs if reading from the file is not possible.
+	 */
+	public GenericMap (@NonNull File file) throws IOException {
+		this (IMapInstructionRegistry.DEFAULT, file);
 	}
 
 	/**
@@ -159,6 +264,20 @@ public class GenericMap implements IMap {
 	 */
 	@Override
 	public Map<IMapInstruction, IMapInstruction> getInstructionMap (MapInstructionCategory category) {
+		// generate map
+		if (category == null) {
+			// create empty map
+			Map<IMapInstruction, IMapInstruction> map = new HashMap<> ();
+
+			// append all elements
+			for (Map.Entry<MapInstructionCategory, Map<IMapInstruction, IMapInstruction>> entry : this.instructionMap.rowMap ().entrySet ()) {
+				map.putAll (entry.getValue ());
+			}
+
+			// return finished map
+			return map;
+		}
+
 		return this.instructionMap.row (category);
 	}
 
@@ -297,6 +416,48 @@ public class GenericMap implements IMap {
 			streamWriter = new OutputStreamWriter (outputStream);
 			writer = new BufferedWriter (streamWriter);
 
+			// get date format
+			SimpleDateFormat format = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm'Z'");
+			format.setTimeZone (TimeZone.getTimeZone ("UTC"));
+
+			// create list
+			List<String> elementList = new ArrayList<> ();
+
+			// append elements
+			elementList.add ("Umbrella Map");
+			elementList.add (null);
+			elementList.add ("Generated on:  " + format.format ((new Date ())));
+			elementList.add ("Generator: " + (this.getClass ().getCanonicalName ().length () % 2 > 1 ? " " : "") + this.getClass ().getCanonicalName ());
+			elementList.add ("Map Version: " + (VERSION != null && VERSION.length () % 2 > 1 ? " " : "") + (VERSION != null ? VERSION : " Development Snapshot"));
+			elementList.add (null);
+
+			// get maximum length
+			int length = 0;
+			for (String element : elementList) length = Math.max (length, (element != null ? element.length () : 0));
+
+			// write header
+			for (String element : elementList) {
+				writer.write ("// ");
+
+				if (element != null) {
+					// calculate spacer
+					int spacerWidth = ((length / 2) - (element.length () / 2));
+
+					// write spacer
+					for (int i = 0; i < spacerWidth; i++) writer.write (" ");
+
+					// write element
+					writer.write (element);
+
+					// write spacer
+					for (int i = 0; i < spacerWidth; i++) writer.write (" ");
+				} else
+					for (int i = 0; i < length; i++) writer.write ("-");
+
+				// write end
+				writer.write (" \\\\\n");
+			}
+
 			// write elements
 			for (Map.Entry<IMapInstruction, IMapInstruction> entry : this.getInstructionMap ().entrySet ()) {
 				// write key identifier
@@ -317,6 +478,9 @@ public class GenericMap implements IMap {
 				// write new line
 				writer.write ("\n");
 			}
+
+			// write eof comment
+			writer.write ("// EOF");
 		} finally {
 			IOUtility.closeQuietly (writer);
 			IOUtility.closeQuietly (streamWriter);
